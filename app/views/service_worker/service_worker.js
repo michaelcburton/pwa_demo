4c8c26ca4dada0f1dc03dab39e6158006f207dd6
Michael Burton <michael.burton@entangla.com>
2024-09-04 19:40:12 -0700

    static resource cache with warming

--- app/views/service_worker/service_worker.js
+++ app/views/service_worker/service_worker.js
@@ -1,52 +1,18 @@
-const version = '1.0.25'; // Update this version number with each change
-
-const ASSETS_TO_CACHE = [
-  'https://unpkg.com/dexie/dist/dexie.js',
-  'https://maxbeier.github.io/tawian-frontend/tawian-frontend.css',
-  'https://fonts.googleapis.com/css?family=Cousine:400,400i,700,700i',
-];
-
-const PAGES_TO_CACHE = [
-  '/',
-  '/offline.html',
-  '/posts',
-  '/manifest.json',
-];
+const version = '1.0.26'; // Update this version number with each change
 
 importScripts(
   "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
 );
 
 // We first define the strategies we will use and the registerRoute function
-const {CacheFirst, NetworkFirst} = workbox.strategies;
+const {CacheFirst, NetworkFirst, NetworkOnly} = workbox.strategies;
 const {registerRoute} = workbox.routing;
-
-// If we have critical pages that won't be changing very often, it's a good idea to use cache first with them
-// registerRoute(
-//   ({url}) => url.pathname.startsWith('/'),
-//     new CacheFirst()
-// )
-
-// For every other page we use network first to ensure the most up-to-date resources
-registerRoute(
-  ({request, url}) => request.destination === "document" ||
-                      request.destination === "",
-  new NetworkFirst()
-)
-
-// For assets (scripts and images), we use cache first
-registerRoute(
-  ({request}) =>  request.destination === "script" ||
-                  request.destination === "style" ||
-                  request.destination === "image",
-  new CacheFirst()
-)
-const {warmStrategyCache} = workbox.recipes;
+const {warmStrategyCache, staticResourceCache} = workbox.recipes;
 const {setCatchHandler} = workbox.routing;
 const strategy = new NetworkFirst();
 const urls = [
-  '/',
   '/offline.html',
+  '/',
   '/posts',
   '/manifest.json',
   'https://unpkg.com/dexie/dist/dexie.js',
@@ -56,6 +22,52 @@ const urls = [
 // Warm the runtime cache with a list of asset URLs
 warmStrategyCache({urls, strategy});
 
+// Static resources cache 
+// The static resources cache recipe allows your service worker to respond
+// to a request for static resources, specifically CSS, JavaScript, and Web 
+// Worker requests, with a stale-while-revalidate caching strategy so those 
+// assets can be quickly served from the cache and be updated in the background
+//
+// This recipe supports cache warming through the warmCache option. See the static 
+// resources cache options for a list of all configuration options.
+staticResourceCache({
+  warmCache: urls,  // Pre-cache the static resources
+  cacheName: `static-resources-${version}`, // Optional: specify a custom cache name
+  plugins: [
+    new workbox.expiration.ExpirationPlugin({
+      maxEntries: 100,  // Optional: limit the number of entries
+      maxAgeSeconds: 30 * 24 * 60 * 60, // Cache static resources for 30 days
+    }),
+  ],
+});
+
+// If we have critical pages that won't be changing very often, it's a good idea to use cache first with them
+// registerRoute(
+//   ({url}) => url.pathname.startsWith('/'),
+//     new CacheFirst()
+// )
+
+// Specific route for the network check image
+registerRoute(
+  ({url}) => url.pathname === '/1x1.png',
+  new NetworkOnly()
+);
+
+// For every other page we use network first to ensure the most up-to-date resources
+registerRoute(
+  ({request, url}) => request.destination === "document" ||
+                      request.destination === "",
+  new NetworkFirst()
+)
+
+// For assets (scripts and images), we use cache first
+// registerRoute(
+//   ({request}) =>  request.destination === "script" ||
+//                   request.destination === "style" ||
+//                   request.destination === "image",
+//   new CacheFirst()
+// )
+
 // Trigger a 'catch' handler when any of the other routes fail to generate a response
 setCatchHandler(async ({event}) => {
   switch (event.request.destination) {
@@ -64,16 +76,4 @@ setCatchHandler(async ({event}) => {
     default:
      return Response.error();
    }
-});
-
-self.addEventListener('install', (event) => {
-  console.log('[Serviceworker]', "install", event)
-});
-
-self.addEventListener('activate', (event) => {
-  console.log('[Serviceworker]', "activate", event)
-});
-
-self.addEventListener('fetch', (event) => {
-  console.log('[Serviceworker]', "fetch", event)
 });
