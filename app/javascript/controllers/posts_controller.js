c00d17747ccfc4bd1bb43d0d77ec54f38d95f967
Michael Burton <michael.burton@entangla.com>
2024-09-05 22:29:32 -0700

    remove debugger

--- app/javascript/controllers/posts_controller.js
+++ app/javascript/controllers/posts_controller.js
@@ -44,7 +44,6 @@ export default class extends Controller {
     };
 
     checkOnlineStatus(isOnLine => {
-      debugger;
       if (isOnLine) {
         console.log("Sending to server...");
         this.sendToServer(data);
